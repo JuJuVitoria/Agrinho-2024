388d2e9207db4b3dbac1a71245fc7f4bca39719f
Julia Vitoria Da Luz <juliavitoriadaluz5@gmail.com>
2024-08-04 21:42:26 -0300

    Criando sobre mim e slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { setupNav } from './modules/nav.js';
+import { setupSlider } from './modules/slider.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     setupNav();
+    setupSlider();
 });
